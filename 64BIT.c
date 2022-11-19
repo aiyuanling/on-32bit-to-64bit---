88a7d36e6e2e96be730fc86fffa5ce3bd0c6281c
aiyuanling <89833118+aiyuanling@users.noreply.github.com>
2022-11-19 10:21:51 +0800

    Update 64BIT.c

--- 64BIT.c
+++ 64BIT.c
@@ -1,4 +1,37 @@
+/*
+#include <stdio.h>
+#include <limits.h>
 
+#if (CHAR_BIT==8)
+	typedef  char                   int8_t  ;
+	typedef  unsigned char          uint8_t ;
+#else
+	#error sizeof(char)不等于1字节
+#endif
+#if (USHRT_MAX==65535)
+	typedef  short                  int16_t ;
+	typedef  unsigned short         uint16_t;
+#else
+	#error sizeof(short)不等于2字节
+#endif
+
+#if (UINT_MAX==4294967295)
+	typedef  int                    int32_t ;
+	typedef  unsigned int           uint32_t;
+#elif (ULONG_MAX==4294967295)
+	typedef  long                   int32_t ;
+	typedef  unsigned long          uint32_t;
+#else 
+	#error sizeof(int)或sizeof(long) 不等于4字节
+#endif
+#if (LONG_MAX==9223372036854775807)
+	typedef  long                   int64_t ;
+	typedef  unsigned long          uint64_t;
+#else 
+	//#error 不支持 64位
+#endif
+*/
+////////////////////////////////////////////////////////////////////////////////
 struct STT_TAG_64BIT
 {
     uint16_t L_L;
@@ -7,7 +40,13 @@ struct STT_TAG_64BIT
     uint16_t H_H;
 } ;
 typedef struct STT_TAG_64BIT STT_64BIT;
-
+char cmp_64BIT(STT_64BIT A,STT_64BIT B)
+{
+	return  (( (*( ((uint32_t *)(&(A))) +1)) > (*( ((uint32_t *)(&(B))) +1))   )  ?  1 :                                                   
+		    (( (*( ((uint32_t *)(&(A))) +1)) < (*( ((uint32_t *)(&(B))) +1))   )  ? -1 :
+			(( (*( ((uint32_t *)(&(A)))   )) > (*( ((uint32_t *)(&(B)))   ))   )  ?  1 : 
+			(( (*( ((uint32_t *)(&(A)))   )) < (*( ((uint32_t *)(&(B)))   ))   )  ? -1 : 0 ))));
+}
 STT_64BIT sub_64BIT(const STT_64BIT  A,const STT_64BIT  B)
 {
 	STT_64BIT ret={0,0,0,0};
@@ -38,102 +77,54 @@ STT_64BIT add_64BIT(const STT_64BIT  A,const STT_64BIT  B)
 	return ret;
 }
 
-// //3212/X/2=41/2
-// // 若M为偶数
-// // M*N=(M/2)*2N
-// // 若M为奇数
-// // M*N=(M/2)*2N+N
-// STT_64BIT mul_64BIT(const STT_64BIT  A,const STT_64BIT  B)
-// {
-// 	STT_64BIT *  m,n,t;
-// 	STT_64BIT ret={0,0,0,0};
-//  if(!( ((*( ((uint32_t *)(&(A))) +1)) >0 || (*( ((uint32_t *)(&(A))) )) >0)&&
-//   	   ((*( ((uint32_t *)(&(B))) +1)) >0 || (*( ((uint32_t *)(&(B))) )) >0) )
-//   ){
-//  	return ret;
-//  };
-// if( (*( ((uint32_t *)(&(A))) +1))>(*( ((uint32_t *)(&(B))) +1)) 
-//        ){
-// 	m=&A;
-// 	n=&B;
-// }else{
-// 	m=&B;
-// 	n=&A;
-// };
-
-//   while ((*( ((uint32_t *)((m))) )) != 1 || (*( ((uint32_t *)((m))) +1))>0  ) {
-//     if (m & 1) {
-//       t += n;
-//     }
-//     m >>= 1;
-//     n <<= 1;
-//   }
 
 
+STT_64BIT  __div64_32(STT_64BIT *n, uint32_t base)
+{
+	STT_64BIT rem = *n;
+	STT_64BIT b ;
+	STT_64BIT res={0,0,0,0}, d = {1,0,0,0};
+	uint32_t flag=0;
+	uint32_t high = (*( ((uint32_t *)(n)) +1));
+	(*( ((uint32_t *)(&(b)))   ))=base;
+	if (high >= base) {
+		high /= base;
+		(*( ((uint32_t *)(&(res))) +1))=high;
+		(*( ((uint32_t *)(&(rem))) +1)) -= (high*base);
+	};
+	while(((!((*( ((uint32_t *)(&(rem))) +1))==0 && (*( ((uint32_t *)(&(rem))) ))==0))&&
+		((*( ((int32_t *)(&(rem))) +1)) >=0))
+                    &&(cmp_64BIT(b,rem)<0) ){
+		b=add_64BIT(b,b);
+		d=add_64BIT(d,d);
+	};
+	do {
+		if( cmp_64BIT(rem,b) >=0 ){
+			rem = sub_64BIT(rem,b);
+			res = add_64BIT(res,d);
+		};
+		flag=((*( ((uint32_t *)(&(b)))+1))&0X01);
+		(*( ((uint32_t *)(&(b)))+1)) >>= 1;
+		(*( ((uint32_t *)(&(b)))  )) >>= 1;
+		(*( ((uint32_t *)(&(b)))  )) +=  (flag<<31) ;
+		flag=((*( ((uint32_t *)(&(d)))+1))&0X01);
+		(*( ((uint32_t *)(&(d)))+1)) >>= 1;
+		(*( ((uint32_t *)(&(d)))  )) >>= 1;
+		(*( ((uint32_t *)(&(d)))  )) +=  (flag<<31) ;
+	} while ( (!((*( ((uint32_t *)(&(d))) +1))==0 && (*( ((uint32_t *)(&(d))) ))==0)) );
 
-// }
-
-// /* 
-//  * unsigned 64位除法，需要的得到余数
-//  * Param - u64	: 被除数
-//  * Param - u32	: 除数
-//  * Param - u32* : 除后的余数
-//  * Return - u64	: 除后的结果
-//  */
-// uint64_t div_u64_rem(uint64_t dividend, uint32_t divisor, uint32_t *remainder)
-// {
-// 	*remainder = __div64_32(dividend, divisor);
-// 	return dividend;
-// }
-
-// uint32_t  __div64_32(uint64_t *n, uint32_t base)
-// {
-// 	uint64_t rem = *n;
-// 	uint64_t b = base;
-// 	uint64_t res, d = 1;
-// 	uint32_t high = rem >> 32;
-
-// 	 Reduce the thing a bit first 
-//     /* 先算高32位，再算低32位，利用(a+b)/c = a/c + b/c的原理*/
-// 	res = 0;
-// 	if (high >= base) {
-//         /* 当被除数的高32位大于除数时，利用(a*b)/c =a / c * b的除法定理  */
-// 		high /= base;
-// 		res = (uint64_t) high << 32;
-// 		rem -= (uint64_t) (high*base) << 32;
-// 	}
-   
-//     /*这里为什么不直接 rem / b 来得到商和余数？因为这时候的rem还是大于32位的 */
-// 	while ((int64_t)b > 0 && b < rem) {
-//         /* 
-//          通过循环倍增算出被除数由多少个除数相加得到，比如被除数是20，除数是3，
-//          第一次循环b = 3,d = 1,rem = 20,第二次循环b = 6,d = 2,rem = 20,
-//          第三次循环b = 12, d = 4, rem = 20,第四次循环 b = 24, d = 8 , rem = 20,循环结束 
-//          n次循环
-//          b = b*(2^(n-1))
-//          d = 2^(n - 1) 
-//         */
-// 		b = b+b;
-// 		d = d+d;
-// 	}
-
-// 	do {
-//         /*
-//          上面的循环结束后，b = 24, d = 8, rem = 20进入这个循环体
-//          第二次循环b = 12, d = 4, rem = 20
-//          第三次循环b = 6 , d = 2, rem = (20 - 12) = 8, 商res += 4 = 4；
-//          第四次循环b = 3 , d = 1, rem = (8 - 6) = 2, 商res += 2 = 6；
-//          第五次循环b = 1 , d = 0, rem = 2, 商res = 6。循环结束
-//          就这样算出了20 / 3 = 6……2的结果
-//         */
-// 		if (rem >= b) {
-// 			rem -= b;
-// 			res += d;
-// 		}
-// 		b >>= 1;
-// 		d >>= 1;
-// 	} while (d);
-
-// 	*n = res;
-// 	return rem;
-// }
+	*n = res;
+	return rem;
+}
+/* 
+ * unsigned 64位除法，需要的得到余数
+ * Param - u64	: 被除数
+ * Param - u32	: 除数
+ * Param - u32* : 除后的余数
+ * Return - u64	: 除后的结果
+ */
+STT_64BIT div_u64_rem(STT_64BIT dividend, uint32_t divisor, STT_64BIT *remainder)
+{
+	*remainder = __div64_32(&dividend, divisor);
+	return dividend;
+}
